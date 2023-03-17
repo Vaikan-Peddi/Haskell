89c5c7e875e3abfdececdf5b08646ec505cf77cf
Vaikan Peddi <95743210+Vaikan-Peddi@users.noreply.github.com>
2023-03-18 00:20:47 +0530

    Update ninetynine.hs

--- ninetynine.hs
+++ ninetynine.hs
@@ -3,3 +3,10 @@
 myLast :: [a] -> a
 myLast [x] = x
 myLast (x:xs) = myLast xs
+
+
+-- Problem 2: Find all but the last element of a list:
+
+myButLast :: [a] -> [a]
+myButLast [x] = []
+myButLast (x:xs) = x : myButLast xs
